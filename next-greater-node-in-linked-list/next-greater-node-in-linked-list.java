4b065c1fee06be27588cceb338337597755eaf7d
Dev Sharma <sharma.dev4242@gmail.com>
2021-09-10 18:28:18 +0530

    Time: 25 ms (38.68%), Space: 81.6 MB (5.02%) - LeetHub

---
+++ next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> A = new ArrayList<>();
+        for (ListNode node = head; node != null; node = node.next) A.add(node.val);
+        int[] res = new int[A.size()];
+        for(int i = 0; i < A.size(); ++i) res[i] = A.get(i);
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = A.size() - 1; i >= 0; --i) {
+            int val = res[i];
+            
+            while(!stack.isEmpty() && stack.peek() <= res[i]) stack.pop();
+            // System.out.println(i + " " + stack.peek());
+            if(stack.isEmpty()) res[i] = 0;
+            else res[i] = stack.peek();
+            
+            stack.push(val);
+        }
+        
+        return res;
+    }
+}
