d2e63121d0569055c19a95c40b2360685b97f89d
Dev Sharma <sharma.dev4242@gmail.com>
2021-09-19 14:01:19 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (73.45%) - LeetHub

---
+++ binary-search/binary-search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int mid;
+        while(l <= r) {
+            mid = (l + r) / 2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[mid] < target) l = mid+1;
+            else r = mid-1;
+        }
+        
+        return -1;
+    }
+}
