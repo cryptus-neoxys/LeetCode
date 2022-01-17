b5b584c732fe91e2f9e17c2e97c9076c06dc8310
Dev Sharma <sharma.dev4242@gmail.com>
2022-01-17 12:54:59 +0530

    Time: 1 ms (86.67%), Space: 38.5 MB (31.61%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        Map<String, String> kMap = new HashMap<>();
-        Map<String, String> wMap = new HashMap<>();
+        Map hm = new HashMap();
         String[] words = s.split(" ");
-        String[] keys = pattern.split("");
+        if (words.length != pattern.length())
+            return false;
         
-        int n = words.length;
-        if(keys.length != n) return false;
-        for(int i = 0; i < n; ++i) {
-            // System.out.println(keys[i] + " " + words[i]);
-            kMap.putIfAbsent(keys[i], words[i]);
-            wMap.putIfAbsent(words[i], keys[i]);
-            if(!kMap.get(keys[i]).equals(words[i])) return false;
-            if(!wMap.get(words[i]).equals(keys[i])) return false;
+        
+        for(Integer i = 0; i < words.length; ++i) {
+            if(hm.put(pattern.charAt(i), i) != hm.put(words[i], i))
+                return false;
+            
+            // _DEBUG
+            // System.out.println(pattern.charAt(i) +  ": " + hm.put(pattern.charAt(i), i) + "\n"+ words[i] + ": " + hm.put(words[i], i));
         }
         
         return true;
