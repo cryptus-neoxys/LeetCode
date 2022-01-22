2968947dc9d2cf2e8827dbe468e9a66d8ad17443
Dev Sharma <sharma.dev4242@gmail.com>
2022-01-22 21:42:36 +0530

    Time: 2 ms (100.00%), Space: 53.1 MB (20.00%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
@@ -2,16 +2,16 @@ class Solution {
     public int numberOfArrays(int[] differences, int lower, int upper) {
         int N = differences.length;
         
-        int[] hidden = new int[N + 1];
+        // int[] hidden = new int[N + 1];
         
-        hidden[0] = 0;
+        int hidden = 0;
         int minIndex = 0;
         int maxIndex = 0;
         
         for(int i = 0; i < N; ++i) {
-            hidden[i + 1] = hidden[i] + differences[i];
-            if(hidden[i + 1] > hidden[maxIndex]) maxIndex = i + 1;
-            if(hidden[i + 1] < hidden[minIndex]) minIndex = i + 1;
+            hidden = hidden + differences[i];
+            if(hidden > maxIndex) maxIndex = hidden;
+            if(hidden < minIndex) minIndex = hidden;
         }
         
         // // _DEBUG
@@ -21,8 +21,8 @@ class Solution {
         // System.out.println("hm = " + hidden[minIndex] + " hM = " +  hidden[maxIndex]);
         // System.out.print("\n");
         
-        if(Math.abs(hidden[maxIndex] - hidden[minIndex]) > Math.abs(upper - lower)) return 0;
-        return (Math.abs(Math.abs(lower - upper) - Math.abs(hidden[maxIndex] - hidden[minIndex])) + 1);
+        if(Math.abs(maxIndex - minIndex) > Math.abs(upper - lower)) return 0;
+        return (Math.abs(Math.abs(lower - upper) - Math.abs(maxIndex - minIndex)) + 1);
         
         // return 0;
     }
