2cbb2f29e7cc56ff2d3a85a4d1fb7829ffe87a2f
Dev Sharma <sharma.dev4242@gmail.com>
2022-01-24 19:17:23 +0530

    Time: 0 ms (100.00%), Space: 37.5 MB (61.97%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -4,7 +4,8 @@ class Solution {
         char[] chArr = s.toCharArray();
         
         for(int i = 0; i < N; ++i) {
-            chArr[i] = Character.toLowerCase(chArr[i]);
+            if(chArr[i] >= 65 && chArr[i] <= 90) chArr[i] += 32;
+            // chArr[i] = Character.toLowerCase(chArr[i]);
         }
         
         return new String(chArr);
