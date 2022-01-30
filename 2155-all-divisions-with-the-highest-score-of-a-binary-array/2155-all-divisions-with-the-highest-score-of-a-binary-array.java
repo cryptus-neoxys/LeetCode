f5cf53e50ae58144af97fae576cac3554e75a1e9
Dev Sharma <sharma.dev4242@gmail.com>
2022-01-30 10:16:31 +0530

    Time: 37 ms (57.14%), Space: 128.6 MB (28.57%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<Integer> maxScoreIndices(int[] nums) {
-        int N = nums.length;
-        List<Integer> res = new ArrayList<>();
-        
-		// create the prefix array
-        int[] pref = new int[N + 1];
-        pref[0] = 0;
-        for(int i = 0; i < N; ++i) pref[i+1] = nums[i] + pref[i];
-        
-		// initialise maxScore to -1
-        int maxScore = -1;
-        int onesToRight, zeroesToLeft, currScore;
-        for(int i = 0; i < N + 1; ++i) {
-		
-            onesToRight = pref[N] - pref[i];
-            zeroesToLeft = i - pref[i];
-            currScore = zeroesToLeft + onesToRight;
-            
-			// current score = zeroesToLeft + onesToRight
-            if(currScore > maxScore) {
-				// reset result
-                res.clear();
-				// set score
-                maxScore = currScore;
+        int oneCount=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==1){
+                oneCount++;
             }
-			
-            if(currScore == maxScore) res.add(i);                        
         }
-        return res;
+
+        int maxDivisor = oneCount;
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+        int runningCount = oneCount;//1
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                runningCount++;
+            } else if (nums[i] == 1) {
+                runningCount--;
+            }
+            if (runningCount >= maxDivisor) {
+                if(runningCount != maxDivisor) {
+                    result.clear();
+                    maxDivisor = runningCount;
+                }
+                result.add(i+1);
+            }
+
+        }
+
+        return result;
     }
 }
