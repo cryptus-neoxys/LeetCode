295c16b4c10f8e3be323367e1c8ef4f5c92c00f4
Dev Sharma <sharma.dev4242@gmail.com>
2022-02-17 19:41:14 +0530

    Time: 9 ms (30.25%), Space: 44.8 MB (27.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -6,17 +6,18 @@ class Solution {
         this.res = new ArrayList<>();
         Arrays.sort(candidates);
         helper(new ArrayList<>(), candidates, target, 0);
-        return res;
+        return this.res;
     }
     
     // backtrack
     private void helper(List<Integer> temp, int[] nums, int target, int idx) {
         if(target == 0) this.res.add(new ArrayList(temp));
-        else if(target < 0) return;
-        for(int i = idx; i < nums.length; ++i) {
-            temp.add(nums[i]);
-            helper(temp, nums, target - nums[i], i);
-            temp.remove(temp.size() - 1);
-        }
+        if(target < 0) return;
+        else
+            for(int i = idx; i < nums.length; ++i) {
+                temp.add(nums[i]);
+                helper(temp, nums, target - nums[i], i);
+                temp.remove(temp.size() - 1);
+            }
     }
 }
