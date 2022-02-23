8e39446d5247c9f8af9f1469cd681f021a91b305
Dev Sharma <sharma.dev4242@gmail.com>
2022-02-23 12:07:08 +0530

    Time: 43 ms (45.75%), Space: 42.9 MB (31.95%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -22,21 +22,43 @@ class Solution {
     
     private Map<Integer, Node> vis;
     
-    private Node helper(Node node) {
-        if(node == null) return null;
-        if(vis.containsKey(node.val)) return vis.get(node.val);
+//     private Node helper(Node node) {
+//         if(node == null) return null;
+//         if(vis.containsKey(node.val)) return vis.get(node.val);
         
-        Node res = new Node(node.val, new ArrayList<Node>());
-        vis.put(res.val, res);
-        for(Node nbr: node.neighbors) {
-            res.neighbors.add(helper(nbr));
+//         Node res = new Node(node.val, new ArrayList<Node>());
+//         vis.put(res.val, res);
+//         for(Node nbr: node.neighbors) {
+//             res.neighbors.add(helper(nbr));
+//         }
+        
+//         return res;
+//     }
+    
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        Node res = new Node(node.val);
+        
+        Queue<Node> q = new LinkedList<>();
+        HashMap<Node, Node> vis = new HashMap<>();
+        q.add(node);
+        vis.put(node, res);
+        
+        while(!q.isEmpty()) {
+            Node n = q.poll();
+            
+            for(Node nbr: n.neighbors) {
+                if(!vis.containsKey(nbr)) {
+                    vis.put(nbr, new Node(nbr.val));
+                    q.add(nbr);
+                }
+                vis.get(n).neighbors.add(vis.get(nbr));
+            }
+            
         }
         
         return res;
-    }
-    
-    public Node cloneGraph(Node node) {
-        this.vis = new HashMap<Integer, Node>();
-        return helper(node);
+        
+        
     }
 }
