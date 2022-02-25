a3444d84c9b07692f0ec32e57c6479b398cf20f4
Dev Sharma <sharma.dev4242@gmail.com>
2022-02-25 20:48:11 +0530

    Time: 2 ms (54.25%), Space: 41.8 MB (30.89%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,24 +1,14 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1S = version1.split("\\.");
-        String[] v2S = version2.split("\\.");
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
         
-        int t = v1S.length - 1;
-        while(t > 0 && Integer.parseInt(v1S[t]) == 0) t--;
-        int[] v1 = new int[t+1];
-        for(int i = 0; i <= t; ++i) v1[i] = Integer.parseInt(v1S[i]);
-
-        t = v2S.length - 1;
-        while(t > 0 && Integer.parseInt(v2S[t]) == 0) t--;
-        int[] v2 = new int[t+1];
-        for(int i = 0; i <= t; ++i) v2[i] = Integer.parseInt(v2S[i]);
-        
-        
-        for(int i = 0; i < Math.min(v1.length, v2.length); ++i) {
-            if(v1[i] != v2[i]) return v1[i] > v2[i] ? 1 : -1;
+        for(int i = 0; i < Math.max(v1.length, v2.length); ++i) {
+            int n1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int n2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if(n1 != n2) return n1 > n2 ? 1 : -1;
         }
         
-        if(v1.length != v2.length) return v1.length > v2.length ? 1 : -1;
         return 0;
     }
 }
