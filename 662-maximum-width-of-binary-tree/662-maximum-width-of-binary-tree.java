304ac27ec4adc0c732f163da2ee4cb405eb75085
Dev Sharma <sharma.dev4242@gmail.com>
2022-02-27 16:26:45 +0530

    Time: 6 ms (9.76%), Space: 44.7 MB (6.61%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     
-    static class Pair {
+    static class Pair {                                         //Pair holds array index of node
         TreeNode node;
         Integer pos;
         
@@ -30,27 +30,26 @@ class Solution {
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(root, 1));
         int res = 1;
-        while(!q.isEmpty()) {
+        while(!q.isEmpty()) {                                   //BFS
             int s = q.size();
             ArrayList<Pair> t = new ArrayList<>();
             while(s-- > 0) {
                 Pair p = q.poll();
                 TreeNode node = p.node;
-                Integer pos = p.pos;
+                Integer pos = p.pos;                            // if   root @  idx=1   ( n )
                 if(node.left != null) {
-                    q.add(new Pair(node.left, 2*pos));
+                    q.add(new Pair(node.left, 2*pos));          // then left @  idx=2   (2n+0)
                     t.add(new Pair(node.left, 2*pos));
                 }
                 if(node.right != null) {
-                    q.add(new Pair(node.right, 2*pos + 1));
+                    q.add(new Pair(node.right, 2*pos + 1));     // and  right @ idx=3   (2n+1)
                     t.add(new Pair(node.right, 2*pos + 1));
                 }
             }
             if(t.size() > 0) {
                 Pair left = t.get(0);
                 Pair right = t.get(t.size() - 1);
-                res = Math.max(res, right.pos - left.pos + 1);
-                // System.out.println(res);
+                res = Math.max(res, right.pos - left.pos + 1);  // size of a level = r - l + 1
             }
         }
     return res;
