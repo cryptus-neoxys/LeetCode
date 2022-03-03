8b6d6f4f771022703a6109afe7c4af5104127124
Dev Sharma <sharma.dev4242@gmail.com>
2022-03-03 17:46:52 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (41.57%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -3,11 +3,10 @@ class Solution {
         if(nums.length < 3) return 0;
         int res = 0, len = 0;
         for(int i = 1; i < nums.length - 1; ++i) {
-            while(i < nums.length - 1 && nums[i] - nums[i-1] == nums[i+1] - nums[i]) {
-                i++; len++;
-            }
-            res += (len * (len + 1)) / 2;
-            len = 0;
+            if(nums[i] - nums[i-1] == nums[i+1] - nums[i]) {
+                len+= 1;
+                res += len;
+            } else len = 0;
         }
         
         return res;
