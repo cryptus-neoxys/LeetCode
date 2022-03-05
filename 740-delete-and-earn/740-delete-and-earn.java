63d8806b3ebd2957b836e5d941d2321e32c2bf03
Dev Sharma <sharma.dev4242@gmail.com>
2022-03-05 23:38:32 +0530

    Time: 11 ms (18.74%), Space: 43.8 MB (46.03%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -3,26 +3,20 @@ class Solution {
         TreeMap<Integer, Integer> map = new TreeMap<>();
 
         for(int n: nums) {
-            map.putIfAbsent(n, 0);
-            map.put(n, map.get(n) + 1);
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        Integer k = map.firstKey();
-        int r1 = k * map.remove(k);
-        if(map.size() == 0) return r1;
-        int k2 = map.firstKey();
-        int r2 = k + 1 == k2 ? Math.max(r1, k2 * map.remove(k2)) : r1 + k2 * map.remove(k2);
-        // System.out.println(map.remove(map.firstKey()) + "\n" + map.remove(map.firstKey()));
-        
+        int p = 0, c = 0, k = -1;
         for(Integer key : map.keySet()) {
-            int t = r2 + key * map.get(key);
-            if(k2 + 1 == key) {
-                t = Math.max(r2, r1 + key * map.get(key));
+            int m = Math.max(p, c);
+            if(k + 1 != key) {
+                c = m + key * map.get(key);
+            } else {
+                c = p + key * map.get(key);
             }
-            r1 = r2;
-            r2 = t;
-            k2 = key;
+            p = m;
+            k = key;
         };
         
-        return r2;
+        return Math.max(p, c);
     }
 }
