e363cf14cdc27f98e34e22ee7c80f74a4f1f4e28
Dev Sharma <sharma.dev4242@gmail.com>
2022-04-10 18:51:52 +0530

    Time: 6 ms (18.46%), Space: 42.1 MB (62.07%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -1,8 +1,10 @@
 class Solution {
     public int calPoints(String[] ops) {
-        Stack<String> st = new Stack<>();
+        Deque<String> st = new ArrayDeque();
+        
         for(int i = 0; i < ops.length; ++i) {
             // System.out.println(ops[i] + ": " + st);
+        
             if(ops[i].equals("+")) {
                 int t = Integer.parseInt(st.pop(), 10);
                 int tm1 = Integer.parseInt(st.pop(), 10);
@@ -10,15 +12,20 @@ class Solution {
                 st.push("" + (t));
                 st.push("" + (t + tm1));
             }
+            
             else if(ops[i].equals("D")) {
                 int t = Integer.parseInt(st.pop());
                 st.push("" + t);
                 st.push("" + (2 * t));
-            } else if(ops[i].equals("C")) {
+            }
+            
+            else if(ops[i].equals("C")) {
                 st.pop();
             }
+            
             else st.push(ops[i]);
         }
+        
         int res = 0;
         while(st.size() != 0) {
             res += Integer.parseInt(st.pop());
