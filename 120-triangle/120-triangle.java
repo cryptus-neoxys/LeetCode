1544d277cc3899c20d9ada7188487603b7e22f8a
Dev Sharma <sharma.dev4242@gmail.com>
2022-06-13 15:39:20 +0530

    Time: 4 ms (61.51%), Space: 43.9 MB (52.83%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,20 +1,19 @@
 class Solution {
     
-    private Integer[][] cache;
-    
-    private int helper(int r, int p, List<List<Integer>> triangle) {
-        if(r >= triangle.size() - 1) return triangle.get(r).get(p);
-        
-        if(cache[r][p] != null) return cache[r][p];
-        
-        cache[r][p] = triangle.get(r).get(p) + Math.min(helper(r + 1, p, triangle), helper(r + 1, p+1, triangle));
-        
-        return cache[r][p];
-    }
-    
     public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle.size() == 0) return 0;
-        cache = new Integer[triangle.size()][triangle.size()];
-        return helper(0, 0, triangle);
+        int N = triangle.size();
+        int[] dp = new int[N];
+        
+        for(int i = 0; i < N; ++i) {
+            dp[i] = triangle.get(N - 1).get(i);
+        }
+        for(int i = N - 2; i >= 0; --i) {
+            for(int j = 0; j < triangle.get(i).size(); ++j) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j+1]);
+            }
+        }
+        
+        return dp[0];
     }
+    
 }
