ddbecb97e278157f551344731e8f790f03a6a0cd
Dev Sharma <sharma.dev4242@gmail.com>
2022-06-29 19:12:25 +0530

    Time: 20 ms (49.21%), Space: 54.4 MB (58.73%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDeletions(String s) {
+        // Store the frequency of each character
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+        }
+        
+        Arrays.sort(frequency);
+        
+        int deleteCount = 0;
+        // Maximum frequency the current character can have
+        int maxFreqAllowed = s.length();
+        
+        for (int i = 25; i >= 0 && frequency[i] > 0; i--) {
+            if (frequency[i] > maxFreqAllowed) {
+                deleteCount += frequency[i] - maxFreqAllowed;
+                frequency[i] = maxFreqAllowed;
+            }
+            maxFreqAllowed = Math.max(0, frequency[i] - 1);
+        }
+        
+        return deleteCount;
+    }     
+}
