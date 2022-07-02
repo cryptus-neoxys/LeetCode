852984a552681f254b7bfe73a3634a2007432de5
Dev Sharma <sharma.dev4242@gmail.com>
2022-07-02 12:43:30 +0530

    Time: 1 ms (62.31%), Space: 41.2 MB (35.40%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.java
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -1,6 +1,5 @@
 class Solution {
     public int sum(int num1, int num2) {
-        if (num2 == 0) return num1;
-        return sum(num1 ^ num2, (num1 & num2) << 1);
+        return num1 + num2;
     }
 }
