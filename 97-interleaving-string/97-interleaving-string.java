bed65dea9cb8094a6c771369349b36b0f2202a00
Dev Sharma <sharma.dev4242@gmail.com>
2022-07-08 02:28:24 +0530

    Time: 5 ms (61.10%), Space: 40.4 MB (96.55%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if ((s1.length()+s2.length())!=s3.length()) return false;
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        if (len1 + len2 != len3) return false;
 
-        boolean[][] matrix = new boolean[s2.length()+1][s1.length()+1];
+        
+        boolean[][] dp = new boolean[len1+1][len2+1];
 
-        matrix[0][0] = true;
-
-        for (int i = 1; i < matrix[0].length; i++){
-            matrix[0][i] = matrix[0][i-1]&&(s1.charAt(i-1)==s3.charAt(i-1));
+        dp[0][0] = true;
+        
+        for(int i = 1; i <= len1; ++i) {
+            dp[i][0] = dp[i-1][0] && s3.charAt(i-1) == s1.charAt(i-1);
         }
-
-        for (int i = 1; i < matrix.length; i++){
-            matrix[i][0] = matrix[i-1][0]&&(s2.charAt(i-1)==s3.charAt(i-1));
+        
+        for(int j = 1;j<=len2;j++) {
+            dp[0][j] = dp[0][j-1] && s3.charAt(j-1) == s2.charAt(j-1);
         }
-
-        for (int i = 1; i < matrix.length; i++){
-            for (int j = 1; j < matrix[0].length; j++){
-                matrix[i][j] = (matrix[i-1][j]&&(s2.charAt(i-1)==s3.charAt(i+j-1)))
-                        || (matrix[i][j-1]&&(s1.charAt(j-1)==s3.charAt(i+j-1)));
+        
+        for(int i = 1; i<=len1; i++) {
+            for(int j = 1; j<=len2; j++) {
+                char c1 = s1.charAt(i - 1);
+                char c2 = s2.charAt(j - 1);
+                char c3 = s3.charAt(i + j - 1);
+                
+                dp[i][j] = (c1 == c3 && dp[i-1][j]) || (c2 == c3 && dp[i][j-1]);
             }
         }
-
-        return matrix[s2.length()][s1.length()];
+        
+        return dp[len1][len2];
+        
     }
 }
